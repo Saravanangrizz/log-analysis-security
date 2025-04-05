53179f4589847e1077763dba01cc75763f696285
Saravanangrizz <112953065+Saravanangrizz@users.noreply.github.com>
2025-04-05 12:38:10 +0530

    api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://localhost:5000', // change for deployment
-});
+axios.get('http://127.0.0.1:5000/api/logs')
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.error('API Error:', err);
+  });
