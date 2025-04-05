e8c5cbebb276ea8eae558e22df31168a7c0cd437
Saravanangrizz <112953065+Saravanangrizz@users.noreply.github.com>
2025-04-05 12:10:00 +0530

    config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 MYSQL_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'Sarogrizz@18',  # 👈 replace this
+    'password': Sarogrizz@18,  # 👈 replace this
     'database': 'log_analysis',
     'port': 3306
 }
