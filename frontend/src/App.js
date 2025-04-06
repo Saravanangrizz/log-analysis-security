22773ed7d7b343d2c62d0e9fdc402c11af6fa906
Saravanangrizz <112953065+Saravanangrizz@users.noreply.github.com>
2025-04-06 20:05:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import LiveChart from "./LiveChart";
+import LiveChart from './components/LiveChart';
 import "./App.css"; // Create this for custom styles
 
 const App = () => {
