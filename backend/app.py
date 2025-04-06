6d09befc6288c7052454eea79fd6e5ae28be44c5
Saravanangrizz <112953065+Saravanangrizz@users.noreply.github.com>
2025-04-06 14:30:55 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -62,5 +62,7 @@ def index():
     return "Log Analysis App is running!"
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    socketio.run(app, host='0.0.0.0', port=port)
+
 
